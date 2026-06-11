f931f3f9556f1743f7d9c6685f4f0d716419bd6b
Refael <refael@gmail.com>
2026-06-11 16:42:48 +0300

    בדיקה האם המילה קיימת

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// console.log(getRandomWord());
-
-
-
 let words;
 let text = "blade";
 let word = text.toUpperCase();
@@ -13,7 +9,7 @@ const game = document.getElementById("game_container");
 let gameOver = false;
 let win = false;
 
-let green = "#588157", yellow = "#a07f1a", gray = "#495057";
+let green = "rgb(88, 129, 87)", yellow = "rgb(160, 127, 26)", gray = "rgb(73, 80, 87)";
 
 
 
@@ -118,46 +114,67 @@ function del() {
 
 function checkWord() {
     if (col == 5) {
+        //האם המילה קיימת
         let temp = "";
         for (let i = 0; i < 5; i++) {
             const curent_box = game.children[row].children[i];
             temp += curent_box.value;
-            let curent_btn = document.getElementById(curent_box.value);
 
-            if (word.charAt(i) == curent_box.value) {
-                curent_box.style.backgroundColor = green;
-                curent_btn.style.backgroundColor = green;
+        }
+        console.log("word:", words.includes(temp.toLowerCase()));
+        if (words.includes(temp.toLowerCase())) {
+
+
+
+
+
+            for (let i = 0; i < 5; i++) {
+
+                const curent_box = game.children[row].children[i];
+                let curent_btn = document.getElementById(curent_box.value);
+                if (word.charAt(i) == curent_box.value) {
+                    curent_box.style.backgroundColor = green;
+                    curent_btn.style.backgroundColor = green;
+                }
+                else if (word.includes(curent_box.value)) {
+                    curent_box.style.backgroundColor = yellow;
+                    if (curent_btn.style.backgroundColor !== green) {
+
+
+                        curent_btn.style.backgroundColor = yellow;
+                    }
+                }
+
+                else {
+                    curent_box.style.backgroundColor = gray;
+                    curent_btn.style.backgroundColor = gray;
+                }
+
+
+
+
+
             }
-            else if (word.includes(curent_box.value)) {
-                curent_box.style.backgroundColor = yellow;
-                curent_btn.style.backgroundColor = yellow;
+            row++;
+            col = 0;
+
+
+
+            if (temp === word) {
+                console.log("win!!")
+                win = true;
+                gameOver = true;
             }
+            else if (row == 6) {
+                console.log("lose :(")
 
-            else {
-                curent_box.style.backgroundColor = gray;
-                curent_btn.style.backgroundColor = gray;
-
+                gameOver = true;
 
             }
         }
-        row++;
-        col = 0;
 
-        if (temp === word) {
-            console.log("win!!")
-            win = true;
-            gameOver = true;
-        }
-        else if (row == 6) {
-            console.log("lose :(")
-
-            gameOver = true;
-
-        }
     }
 
-    else
-        console.log("EROR' not a complea");
 
 
 }
@@ -170,19 +187,6 @@ function isAlphabetic(str) {
 
     return (/^[A-Za-z]+$/.test(str));
 }
-
-
-// document.getElementById("myElement").style.backgroundColor = "yellow";
-
-// const parent = document.querySelector(".myDiv");
-// const secondChild = parent.children[1]; // אינדקס מתחיל מ־0
-
-
-
-
-
-
-
 
 
 
