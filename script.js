7f6dfdabb4d17819f42f8f2e8af549bb05ef657e
Refael <refael@gmail.com>
2026-06-12 19:11:16 +0300

    הוספת פונקציה שמקפיצה הודעות

--- script.js
+++ script.js
@@ -193,5 +193,17 @@ function isAlphabetic(str) {
     return (/^[A-Za-z]+$/.test(str));
 }
 
+function msg(str) {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.textContent = str;
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
 
 
